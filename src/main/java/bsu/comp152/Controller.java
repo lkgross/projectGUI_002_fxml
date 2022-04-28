b933c3a5c6b55adbf3cce303823b9e88528759d6
L1Gross <profgross@gmail.com>
2022-04-28 12:22:24 -0400

    Make a functioning adding machine

--- src/main/java/bsu/comp152/Controller.java
+++ src/main/java/bsu/comp152/Controller.java
@@ -1,4 +1,40 @@
 package bsu.comp152;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+
 public class Controller {
+
+    @FXML
+    private TextField display;
+    @FXML
+    private int firstNumber;
+
+    public void numberClicked(ActionEvent event){
+        var buttonPressed = (Button) event.getSource();
+        var newNumber = buttonPressed.getText();
+        var oldNumber = display.getText();
+        var newDisplayNumber = oldNumber + newNumber;
+        display.setText(newDisplayNumber);
+    }
+
+    public void clearPressed(ActionEvent event){
+        display.setText("");
+    }
+
+    public void plusPressed(ActionEvent event){
+        String numberAsString = display.getText();
+        firstNumber = Integer.parseInt(numberAsString);
+        display.setText("");
+    }
+
+    public void equalsPressed(ActionEvent event){
+        String numberAsString = display.getText();
+        int secondNumber = Integer.parseInt(numberAsString);
+        display.setText(String.valueOf(firstNumber + secondNumber));
+    }
 }
--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,7 +1,35 @@
 package bsu.comp152;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Main -
+ * A class for loading a .fxml file
+ *
+ * Starter code John Santore, jsantore@bridgew.edu
+ *
+ * Modified by: Laura Gross, laura.gross@bridgew.edu, 7/28/2022
+ */
+public class Main extends Application {
     public static void main(String[] args) {
-        // Code!
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Store the location of the user interface (UI) resource.
+        var loc = getClass().getResource("Main.fxml");
+        // Parent is the superclass of Stage.
+        Parent root = FXMLLoader.load(loc);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("A GUI designed in Scene Builder");
+        primaryStage.show();
     }
 }
